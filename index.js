cf093f919faf451a3a68eeab47194352540cab6b
Carson Ouckama <>
2022-03-07 16:53:03 +0000

    Worked and the group object and group post requests

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-function Log(date, log) {
+
+function Group(name) {
+	this.name = name;
+	this.id = alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)];
+	this.logs = [];
+}
+
+function Log(date, log, author) {
 	this.date = date;
 	this.log = log;
+	this.author = author;
 }
 function User(name, password) {
 	this.id = alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)] + alphabet[Math.floor(Math.random()*26)];
@@ -13,6 +21,11 @@ function User(name, password) {
 const express = require('express');
 const app = express();
 var accounts = [];
+
+var carson = new Group('Carson\'s group');
+carson.logs.push("3/07/2022", "Hello World", "Carson")
+
+var groups = [carson];
 
 app.listen(3000, () => console.log('listening at 3000'));
 app.use( express.static( __dirname ) );
@@ -73,4 +86,30 @@ app.post('/newUser', (request, response) => {
 	var i = accounts.length - 1;
 	console.log('{\n id: ' + accounts[i].id + '\n name: ' + accounts[i].name + "\n password: " + accounts[i].password + '\n}')
 	response.send({success: true});
-})
+});
+
+app.post('/getGroupData', (request, response) => {
+	var data = request.body;
+	for (var u in groups) {
+		if (data.name === groups[u].name) {
+			response.send({success: true, logs: groups[u].logs});
+			console.log(groups[u].id + ' grabbed their data.');
+			return;
+		}
+	}
+	response.send({success: false, reason: "Group does not exist!"});
+});
+
+app.post('/addGroupData', (request, response) => {
+	var data = request.body;
+	for (var u in groups) {
+		if (data.name === groups[u].name) {
+			groups[u].logs.push(new Log(data.date, data.log, data.author));
+			console.log("User: " + data.id + ' added data to their group');
+			response.send({success: true});
+			return;
+		}
+	}
+	response.send({success: false, reason: "Group doesn't exist!"});
+	
+});
